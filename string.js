5b9ecf8f29ffaa0e1c194893916b7423ca52ad52
jaina1008 <mailme.anshuljain@gmail.com>
2024-06-21 17:47:41 +1200

    set cache time limit

--- string.js
+++ string.js
@@ -1,8 +1,13 @@
 const client = require('./client')
 
 async function init() {
-    await client.set('mess:6', "Hey from Nodejs")
-    const result = await client.get('mess:6')
+    // Set message:6 value
+    // await client.set('message:6', "Hey from Nodejs")
+
+    // value available to us for 10 seconds as a way to invalidate cache.
+    await client.expire("message:6", 10) 
+
+    const result = await client.get('message:6')
     console.log("Result -> ", result)
 }
 
