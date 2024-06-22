4e91ee8a9171f2eb54433d8ee287f1aa3e31fd12
jaina1008 <mailme.anshuljain@gmail.com>
2024-06-22 18:41:27 +1200

    server setup to cache JSON

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const axios = require('axios').default
+const client = require('./client')
 
 const app = express()
 
 app.get("/", async (req, res) => {
+
+    const cacheValue = await client.get('todos')
+
+    if (cacheValue) return res.json(JSON.parse(cacheValue))
+
     const {data} = await axios.get(
         'https://jsonplaceholder.typicode.com/todos'
     )
+
+    await client.set('todos', JSON.stringify(data))
+    await client.expire('todos', 30)
     return res.json(data)
 })
 
+// listen on port 9000 via postman
 app.listen(9000)
