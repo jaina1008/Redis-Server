15912e65dc07023b292c220b53d07231785b5485
jaina1008 <mailme.anshuljain@gmail.com>
2024-06-22 18:13:42 +1200

    added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const axios = require('axios').default
+
+const app = express()
+
+app.get("/", async (req, res) => {
+    const {data} = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos'
+    )
+    return res.json(data)
+})
+
+app.listen(9000)
